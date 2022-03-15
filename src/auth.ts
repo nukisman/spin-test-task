0b751ec5327f3eeeba175438eddbeb8cc9b62b22
nukisman <nukisman@gmail.com>
2022-03-15 09:00:27 +0300

    Auth modules refactoring

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import { dispatch, getState } from './store.js';
-import { AppWindow } from './types';
+import { AppWindow, State } from './types';
+import { display, html } from './util.js';
 
 declare let window: AppWindow;
 
@@ -10,7 +11,34 @@ export const signIn = async () => {
     .catch(console.error);
 };
 
-export const signOut = () => {
+const signOut = () => {
   window.wallet.signOut();
   dispatch({ type: 'sign', payload: 'out' });
+};
+
+const initView = (state: State): void => {
+  html('sign-in').onclick = signIn;
+  html('sign-out').onclick = signOut;
+
+  const inputAddress = html('wallet-address');
+  inputAddress.oninput = (e: Event) => {
+    // @ts-ignore
+    dispatch({ type: 'address', payload: e.target.value });
+  };
+};
+
+const updateView = (state: State) => {
+  display('signed-in', state.signed === 'in');
+  display('signed-out', state.signed === 'out');
+  display('signed-pending', state.signed === 'pending');
+
+  const postfix = '.testnet';
+  (html('sign-in') as HTMLButtonElement).disabled = !(
+    state.address.length > postfix.length && state.address.endsWith(postfix)
+  );
+};
+
+export const auth = {
+  initView,
+  updateView,
 };
--- src/view.ts
+++ src/view.ts
@@ -1,36 +1,18 @@
-import { signIn, signOut } from './auth.js';
+import { auth } from './auth.js';
 import { balance } from './balance.js';
 import { markets } from './markets.js';
 import { orderBook } from './orderBook.js';
-import { addStateListener, dispatch, getState } from './store.js';
+import { addStateListener, getState } from './store.js';
 import { AppWindow, State } from './types.js';
-import { display, html } from './util.js';
 
 declare let window: AppWindow;
 
 const initView = (state: State): void => {
-  html('sign-in').onclick = signIn;
-  html('sign-out').onclick = signOut;
-
-  const inputAddress = html('wallet-address');
-  inputAddress.oninput = (e: Event) => {
-    // @ts-ignore
-    console.log(e.target.value);
-    // @ts-ignore
-    dispatch({ type: 'address', payload: e.target.value });
-  };
+  auth.initView(state);
 };
 
 const updateView = (state: State): void => {
-  display('signed-in', state.signed === 'in');
-  display('signed-out', state.signed === 'out');
-  display('signed-pending', state.signed === 'pending');
-
-  const postfix = '.testnet';
-  (html('sign-in') as HTMLButtonElement).disabled = !(
-    state.address.length > postfix.length && state.address.endsWith(postfix)
-  );
-
+  auth.updateView(state);
   balance.updateView(state);
   markets.updateView(state);
   orderBook.updateView(state);
